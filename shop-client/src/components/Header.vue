0d68ead2b9926e9cf91a67afaac06c2435e68d18
why <488009667@qq.com>
2022-04-23 19:02:31 +0800

    前端开发主页Header区域主体布局样式完成

---
+++ shop-client/src/components/Header.vue
@@ -0,0 +1,195 @@
+<template>
+  <div id="nav-top">
+    <div id="nav-top-left">
+      <el-breadcrumb separator="" style="padding: 0.7rem;">
+        <el-breadcrumb-item :to="{ path: '/' }" class="nav-top-item">千羽商城</el-breadcrumb-item>
+        <el-breadcrumb-item class="nav-top-item">QY</el-breadcrumb-item>
+        <el-breadcrumb-item class="nav-top-item">云服务</el-breadcrumb-item>
+        <el-breadcrumb-item class="nav-top-item">有品</el-breadcrumb-item>
+        <el-breadcrumb-item class="nav-top-item">千羽开放平台</el-breadcrumb-item>
+        <el-breadcrumb-item class="nav-top-item">企业团购</el-breadcrumb-item>
+        <el-breadcrumb-item class="nav-top-item">资质证照</el-breadcrumb-item>
+        <el-breadcrumb-item class="nav-top-item">协议规则</el-breadcrumb-item>
+        <el-breadcrumb-item class="nav-top-item">下载app</el-breadcrumb-item>
+        <el-breadcrumb-item class="nav-top-item">QUUI</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+
+    <div id="nav-top-mid">
+      <el-breadcrumb separator="" style="padding: 0.7rem;margin-left:15vh">
+        <el-breadcrumb-item :to="{ path: '/' }" class="nav-top-item">登录</el-breadcrumb-item>
+        <el-breadcrumb-item class="nav-top-item">注册</el-breadcrumb-item>
+        <el-breadcrumb-item class="nav-top-item">消息通知</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+
+    <div id="nav-top-right">
+      <div class="shop-car-left">
+        <img alt="未加载" src="../assets/img/shopping-car.png" style="height: 1rem">
+      </div>
+      <div class="shop-car-right">购物车（0）</div>
+    </div>
+  </div>
+
+  <div style="clear: both"></div>
+
+  <div id="nav-bottom">
+    <div class="nav-bottom-logo">
+      <img alt="未加载" src="../assets/img/logo.png" class="img-logo">
+    </div>
+    <div class="nav-bottom-link">
+      <ul>
+        <li><a>手机</a></li>
+        <li><a>笔记本</a></li>
+        <li><a>潮鞋</a></li>
+        <li><a>手表</a></li>
+        <li><a>箱包</a></li>
+        <li><a>生活家居</a></li>
+        <li><a>配饰</a></li>
+        <li><a>美妆</a></li>
+        <li><a>潮玩</a></li>
+        <li><a>艺术</a></li>
+      </ul>
+    </div>
+    <div class="nav-bottom-search">
+      <input type="text" placeholder="笔记本">
+      <div>
+        <img alt="未加载" src="../assets/img/search.png" style="height: 1.5rem;padding: 0.8rem" @mouseout="removeActive">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data(){
+    return{
+      searchImg: "E:\\文件\\毕业设计\\qainyu-shop\\shop-client\\src\\assets\\img\\search.png"
+    }
+  },
+
+  methods: {
+    removeActive(){
+      this.searchImg = "E:\\文件\\毕业设计\\qainyu-shop\\shop-client\\src\\assets\\img\\search.png";
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* 上部导航 */
+#nav-top{
+  height: 2rem;
+  width: 100%;
+  background-color: #262626;
+  color: #bfbfbf;
+  font-size: xx-small;
+}
+#nav-top-left{
+  width: 55%;
+  margin-left: 15vh;
+  float: left;
+}
+#nav-top-mid{
+  float: left;
+}
+#nav-top-right{
+  float: left;
+  height: 100%;
+  background-color: #595959;
+}
+#nav-top-right:hover{
+  background-color: white;
+  color: orange;
+  cursor:pointer;
+}
+.shop-car-left{
+  float: left;
+  padding-top: 0.6rem;
+  padding-left: 1rem;
+}
+.shop-car-right{
+  float: left;
+  margin-left: 0.3rem;
+  padding-top: 0.7rem;
+  padding-right: 1rem;
+}
+.nav-top-item{
+  cursor:pointer;
+  padding-left: 0.6rem;
+  padding-right: 0.3rem;
+}
+.nav-top-item:hover{
+  color: white;
+}
+div{
+  white-space:nowrap;
+}
+
+/* 下部导航 */
+#nav-bottom{
+  height: 6rem;
+}
+.nav-bottom-logo{
+  margin-left: 15vh;
+  padding: 1rem;
+  width: 9rem;
+  float: left;
+}
+.img-logo{
+  height: 4rem;
+}
+.img-logo:hover{
+  height: 3.99rem;
+}
+
+.nav-bottom-link{
+  float: left;
+  height: 2rem;
+  margin-top: 2rem;
+  margin-left: 2rem;
+}
+.nav-bottom-link li{
+  padding: 0.6rem;
+  float: left;
+  height: 1rem;
+  cursor:pointer;
+}
+a{
+  color: #262626;
+}
+.nav-bottom-link li a:hover{
+  color: orange;
+}
+
+/* 搜索框 */
+.nav-bottom-search{
+  float: left;
+  margin-left: 18vh;
+  margin-top: 1.5rem;
+}
+.nav-bottom-search input{
+  height: 3rem;
+  width: 13rem;
+  float: left;
+  border: 1px solid #d9d9d9;
+}
+.nav-bottom-search input:hover{
+  border: 1px solid #bfbfbf;
+}
+.nav-bottom-search input:focus{
+  border: 1px solid #bfbfbf;
+}
+.nav-bottom-search div{
+  float: left;
+  height: 3rem;
+  width: 3rem;
+  border: 1px solid #d9d9d9;
+}
+.nav-bottom-search div:hover{
+  border: 1px solid #bfbfbf;
+  cursor: pointer;
+  background-color: orange;
+}
+</style>
--- shop-client/src/views/Home.vue
+++ shop-client/src/views/Home.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="home">
-    千余商城首页
+  <div class="common-layout">
+    <el-container>
+
+      <!-- 头部导航 -->
+      <el-header>
+        <Header></Header>
+      </el-header>
+      <div style="clear: both"></div>
+
+      <!-- 主体部分 -->
+      <el-main>Main</el-main>
+
+      <!-- 底部链接 -->
+      <el-footer>Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import Header from "../components/Header";
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {Header}
 }
 </script>
