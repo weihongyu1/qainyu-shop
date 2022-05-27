d3e75b61f0983a0e11de3641ad5e4b8ee3c44c2c
why <488009667@qq.com>
2022-05-27 16:21:38 +0800

    后端项目重构

--- shop-server/shop-web/src/main/java/com/why/shopserver/ShopServerApplication.java
+++ shop-server/shop-web/src/main/java/com/why/shopserver/ShopServerApplication.java
@@ -1,4 +1,4 @@
-package com.why.shopserver;
+package com.why.shopServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 public class ShopServerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ShopServerApplication.class, args);
     }
+
 }
